012290fbed0c2b2619d04ee18a7efa64167bcbbf
wasifa026 <wazfiar@gmail.com>
2024-06-30 12:21:11 +0600

    profile_added

--- SmartTech/Controllers/HomeController.cs
+++ SmartTech/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace SmartTech.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
---
+++ SmartTech/Controllers/ProfileController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartTech.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Wishlist()
+        {
+            return View();
+        }
+        public ActionResult Orders()
+        {
+            return View();
+        }
+
+        // GET: Profile/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Profile/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profile/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Profile/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Profile/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
