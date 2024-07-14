c816cfea67bfe8b58f0591577f3d9671ebeb39ff
imehedi007 <olivegreen.mehedi@gmail.com>
2024-07-14 13:46:13 +0600

    userupdated

--- SmartTech/Models/user.cs
+++ SmartTech/Models/user.cs
@@ -20,6 +20,7 @@ namespace SmartTech.Models
         public string number { get; set; }
         public Nullable<System.DateTime> email_verified_at { get; set; }
         public string password { get; set; }
+        public string confirm_password { get; set; }
         public string remember_token { get; set; }
         public Nullable<System.DateTime> created_at { get; set; }
         public Nullable<System.DateTime> updated_at { get; set; }
