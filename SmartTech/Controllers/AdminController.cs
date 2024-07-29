1738b19cbf729ffb32c564c4d5e9b54af4a2e822
imehedi007 <olivegreen.mehedi@gmail.com>
2024-07-29 11:30:42 +0600

    bannerCRUD

--- SmartTech/Controllers/AdminController.cs
+++ SmartTech/Controllers/AdminController.cs
@@ -1,37 +1,113 @@
 ﻿using SmartTech.Models;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace SmartTech.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
         private SmartTechEntities db = new SmartTechEntities();
+
+        // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
+
         public ActionResult Banner()
         {
-            return View();
+            return View(db.banners.ToList());
         }
+
+        [HttpGet]
         public ActionResult AddBanner()
         {
-
             return View();
         }
+
+        [HttpPost]
+        public ActionResult AddBanner(banner banner, HttpPostedFileBase bannerImage)
+        {
+            string[] validImageTypes = { "image/jpeg", "image/jpg", "image/png" };
+
+            if (bannerImage == null || !validImageTypes.Contains(bannerImage.ContentType))
+            {
+                ModelState.AddModelError("banner_image", "Please select a valid image file (jpg, jpeg, png).");
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (bannerImage != null)
+                {
+                    string path = Server.MapPath("~/Content/uploads/frontend/banners/" + bannerImage.FileName);
+                    bannerImage.SaveAs(path);
+                    banner.banner_image = bannerImage.FileName;
+                }
+                db.banners.Add(banner);
+                db.SaveChanges();
+                return RedirectToAction("Banner");
+            }
+            return View(banner);
+        }
+
+        [HttpGet]
+        public ActionResult EditBanner(long id)
+        {
+            var banner = db.banners.Find(id);
+            if (banner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(banner);
+        }
+
+        [HttpPost]
+        public ActionResult EditBanner(banner banner, HttpPostedFileBase bannerImage)
+        {
+            if (ModelState.IsValid)
+            {
+                if (bannerImage != null)
+                {
+                    string path = Server.MapPath("~/Content/uploads/frontend/banners/" + bannerImage.FileName);
+                    bannerImage.SaveAs(path);
+                    banner.banner_image = bannerImage.FileName;
+                }
+
+                /*db.Entry(banner).State = EntityState.Modified;
+                db.banners.Add(banner);*/
+                db.banners.
+                db.SaveChanges();
+                return RedirectToAction("Banner");
+            }
+            return View(banner);
+        }
+
+        public ActionResult DeleteBanner(int id)
+        {
+            var banner = db.banners.Find(id);
+            if (banner == null)
+            {
+                return HttpNotFound();
+            }
+            db.banners.Remove(banner);
+            db.SaveChanges();
+            return RedirectToAction("Banner");
+        }
+
         public ActionResult Shipping()
         {
             return View();
         }
+
         public ActionResult AddCategories()
         {
             return View();
         }
+
         public ActionResult AddProducts()
         {
             return View();
         }
-
     }
 }
